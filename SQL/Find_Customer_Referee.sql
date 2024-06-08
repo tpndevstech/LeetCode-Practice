fcede88ccd0970868211c0dc529ff8ac2b9e0133
Tapan Anand <70485285+tpndevstech@users.noreply.github.com>
2024-06-08 23:40:13 +0530

    Update Find_Customer_Referee.sql

--- SQL/Find_Customer_Referee.sql
+++ SQL/Find_Customer_Referee.sql
@@ -1,2 +1,3 @@
- SELECT name FROM customer                                          
- WHERE referee_id != 2 OR referee_id IS NULL;
+# Write your MySQL query statement below
+SELECT name FROM customer                                          
+WHERE referee_id != 2 OR referee_id IS NULL;
